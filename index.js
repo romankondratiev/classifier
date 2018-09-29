bb1f5fac65d9297a7b4d4f36f3cd2cf9e73bde62
Roman Kondratiev <romhedoo@gmail.com>
2018-09-29 17:29:50 +0200

    test

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-      async function first_model(){
+---
+layout: default
+title: Classifier
+---
+async function first_model(){
       const mobilenet = await tf.loadModel('https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json');
-      //The input size is [null, 224, 224, 3]
-	  const input_s = mobilenet.inputs[0].shape;
-
-	  //The output size is [null, 1000]
-	  const output_s = mobilenet.outputs[0].shape;
-
-	  //mobilenet.predict(tf.zeros([1, 224, 224, 3]))
-	  //await pred.argMax().print();
-      
+      const input_s = mobilenet.inputs[0].shape;
+	const output_s = mobilenet.outputs[0].shape;
       document.getElementById('output_field').innerText = 
       'Your model predicts:\n\n' + 
       mobilenet.predict(tf.zeros([1, 224, 224, 3]))}
       first_model();
+      
